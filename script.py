135c78895b5793d61dd4b6a253691209f6978971
Oliver Jackson <scorj@leeds.ac.uk>
2024-02-26 13:35:27 +0000

    add script file

---
+++ script.py
@@ -0,0 +1,15 @@
+# myscript.py:
+x = {"a": 37, "b": 42, "c": 927}
+y = "hello " + "world"
+
+
+class foo(object):
+    def f(self):
+        return y**2
+
+    def g(self, x: int, y: int = 42) -> int:
+        return x - -y
+
+
+def f(a):
+    return 37 + -a[42 - a : y * 3]
