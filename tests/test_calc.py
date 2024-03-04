57e450689b40269da991a584dd37419c69579cf8
Oliver Jackson <scorj@leeds.ac.uk>
2024-03-04 13:50:48 +0000

    corrections

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,5 +1,5 @@
 
-import function.calc as fc
+import functions.calc as fc
 def test_negatives():
     assert fc.adivb(-10,-5) == 2, "Two negative should return a positive "
 # assert adivb(-9,3) == -3, "One positive one negative should return a negative "
